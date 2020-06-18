dd3df87e57539603659d0b19c5d42f44fdca8510
RPAYAL <rpayal@hotmail.com>
2020-06-19 00:23:48 +0100

    Notes api - component to automatically add username to new note, data initializer on startup

---
+++ notes-api/src/main/kotlin/com/co4gsl/component/AddUserToNote.kt
@@ -0,0 +1,19 @@
+package com.co4gsl.component
+
+import com.co4gsl.entity.Note
+import org.springframework.data.rest.core.annotation.HandleBeforeCreate
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+
+@Component
+@RepositoryEventHandler(Note::class)
+class AddUserToNote {
+
+    @HandleBeforeCreate
+    fun handleCreate(note: Note) {
+        val username: String = SecurityContextHolder.getContext().getAuthentication().name
+        println("Creating note: $note with user: $username")
+        note.user = username
+    }
+}
---
+++ notes-api/src/main/kotlin/com/co4gsl/component/DataInitializer.kt
@@ -0,0 +1,20 @@
+package com.co4gsl.component
+
+import com.co4gsl.entity.Note
+import com.co4gsl.entity.NotesRepository
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Repository
+
+@Component
+class DataInitializer(val repository: NotesRepository) : ApplicationRunner {
+
+    @Throws(Exception::class)
+    override fun run(args: ApplicationArguments?) {
+        listOf("Note 1", "Note 2", "Note 3").forEach {
+            repository.save(Note(title = it, user = "user"))
+        }
+        repository.findAll().forEach { println(it) }
+    }
+}
