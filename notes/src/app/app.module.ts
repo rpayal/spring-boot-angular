93b7615b2ee76ecc7dea917adc84fa3c806b92a4
RPAYAL <rpayal@hotmail.com>
2020-06-20 01:02:18 +0100

    Added Bootstrap to note app

--- notes/src/app/app.module.ts
+++ notes/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { NoteModule } from './note/note.module';
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule,
     AuthRoutingModule,
     NoteModule
